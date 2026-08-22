4f6b5307b25f21286dd24fadbb5911319c44d2cf
Khaotang <tangtangja1234@gmail.com>
2026-08-22 14:48:54 +0700

    Change Architecture from Batch Processing to Real-time Processing and start creating processUploadedImage function

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ import { getFirestore, FieldValue } from "firebase-admin/firestore";
 import { initializeApp } from "firebase-admin/app";
 import { getStorage } from "firebase-admin/storage";
 import { PaddleOcrService } from "ppu-paddle-ocr";
+import { onObjectFinalized } from "firebase-functions/v2/storage";
 
 initializeApp();
 
@@ -25,72 +26,81 @@ async function initializeOcr() {
   }
 }
 
-export const testFunction = onSchedule(
-  { memory: "512MiB", timeoutSeconds: 300 },
+// export const testFunction = onSchedule(
+//   { memory: "512MiB", timeoutSeconds: 300 },
+//   async (event) => {
+//     console.log("Checking for pending OCR jobs...");
+
+//     const snapshot = await db
+//       .collection("certificates")
+//       .where("status", "==", "pending")
+//       .get();
+
+//     console.log(`found ${snapshot.size} pending jobs.`);
+
+//     if (snapshot.empty) {
+//       return;
+//     }
+
+//     await initializeOcr();
+
+//     for (const doc of snapshot.docs) {
+//       const data = doc.data();
+
+//       console.log(data.storagePath);
+
+//       console.log(`processing ${data.fileName}`);
+
+//       await doc.ref.update({
+//         status: "processing",
+//         startedAt: FieldValue.serverTimestamp(),
+//       });
+
+//       try {
+//         const file = bucket.file(data.storagePath);
+
+//         const [buffer] = await file.download();
+
+//         console.log(`Download ${buffer.length} bytes`);
+
+//         const arrayBuffer = buffer.buffer.slice(
+//           buffer.byteOffset,
+//           buffer.byteOffset + buffer.byteLength,
+//         );
+
+//         console.log(`Running OCR on ${data.fileName}`);
+
+//         const result = await ocrService.recognize(arrayBuffer);
+
+//         console.log("OCR result:");
+//         console.log(result.text);
+
+//         await doc.ref.update({
+//           status: "completed",
+//           ocrText: result.text,
+//         });
+
+//         console.log(`completed ${data.fileName}`);
+//       } catch (error) {
+//         console.error("OCR failed", error);
+
+//         await doc.ref.update({
+//           status: "failed",
+//           error: error instanceof Error ? error.message : String(error),
+//           failedAt: FieldValue.serverTimestamp(),
+//         });
+//       }
+//     }
+//   },
+// );
+
+export const processUploadedImage = onObjectFinalized(
+  { memory: "512MiB", timeoutSeconds: 120 },
   async (event) => {
-    console.log("Checking for pending OCR jobs...");
+    const file = event.data;
 
-    const snapshot = await db
-      .collection("certificates")
-      .where("status", "==", "pending")
-      .get();
-
-    console.log(`found ${snapshot.size} pending jobs.`);
-
-    if (snapshot.empty) {
-      return;
-    }
-
-    await initializeOcr();
-
-    for (const doc of snapshot.docs) {
-      const data = doc.data();
-
-      console.log(data.storagePath);
-
-      console.log(`processing ${data.fileName}`);
-
-      await doc.ref.update({
-        status: "processing",
-        startedAt: FieldValue.serverTimestamp(),
-      });
-
-      try {
-        const file = bucket.file(data.storagePath);
-
-        const [buffer] = await file.download();
-
-        console.log(`Download ${buffer.length} bytes`);
-
-        const arrayBuffer = buffer.buffer.slice(
-          buffer.byteOffset,
-          buffer.byteOffset + buffer.byteLength,
-        );
-
-        console.log(`Running OCR on ${data.fileName}`);
-
-        const result = await ocrService.recognize(arrayBuffer);
-
-        console.log("OCR result:");
-        console.log(result.text);
-
-        await doc.ref.update({
-          status: "completed",
-          ocrText: result.text,
-        });
-
-        console.log(`completed ${data.fileName}`);
-
-      } catch (error) {
-
-        console.error("OCR failed", error);
-
-        await doc.ref.update({
-          status: "failed",
-          error: error instanceof Error ? error.message : String(error),
-          failedAt: FieldValue.serverTimestamp(),
-        });
-      }
-    }
+    console.log(file.name);
+    console.log(file.contentType);
+    console.log(file.bucket);
   },
 );
